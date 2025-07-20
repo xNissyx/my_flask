59174032445ba079f20dc73db5003efb04c7f96f
xNissyx <taku210abc123@gmail.com>
2025-07-20 14:24:39 +0900

    [Add] Flask description&basic rooting

--- app.py
+++ app.py
@@ -1,20 +1,31 @@
-# app.py
+"""
+仮想環境に入る用のコピペ
+.\venv\Scripts\activate
+"""
+from flask import Flask         #   Flaskをインポート
 
-from flask import Flask
-
-# Flaskアプリケーションのインスタンスを作成します。
-# __name__ はPythonがモジュールをどのようにロードしているかを示し、
-# Flaskがリソースを見つけるのに役立ちます。
 app = Flask(__name__)
 
-# ルートURL (/) にアクセスしたときに実行される関数を定義します。
+"""
+デコレーター
+()内のURLにアクセスしたときに、その中の関数を実行
+"""
 @app.route('/')
 def hello_world():
-    # ブラウザに表示される文字列を返します。
-    return '<h1>Hello, World! from Flask App!</h1>'
+    return "<h1>ルートページです</h1>"
 
-# このスクリプトが直接実行された場合に、開発サーバーを起動します。
+@app.route('/about')
+def about():
+    return "Aboutページです"
+
+"""
+Flask側の動き
+・@app.route()で指定されたデコレーターの部分を解析
+・<>で囲われた部分はプレースホルダーとして認識し、変数としてビュー関数へ渡す
+"""
+@app.route('/user/<username>')  #   動的なURLを作成する方法
+def show_user_profile(username):
+    return f"{username}さんのページです"
+
 if __name__ == '__main__':
-    # debug=True にすると、コードの変更が自動的にリロードされ、
-    # エラーが発生したときに詳細なデバッグ情報が表示されます。
-    app.run(debug=True)
+    app.run(debug=True)         #   debug=Trueにすると開発時は色々いいらしい
