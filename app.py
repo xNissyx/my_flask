9aeb79293a824c562b095751f37b35bb0b4a672a
xNissyx <taku210abc123@gmail.com>
2025-08-15 06:24:19 +0900

    [add] create&delete

--- app.py
+++ app.py
@@ -2,11 +2,13 @@
 仮想環境に入る用のコピペ
 .\venv\Scripts\activate
 """
-from flask import Flask, render_template         #   Flask, rendertemplatesをインポート
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
+# db_path = os.path.join(app.instance_path, 'site.db')
 # 'sqlite:///site.db' は、プロジェクトのルートに 'site.db' というファイル名でDBを作成するという意味
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
@@ -27,8 +29,38 @@ class User(db.Model):
 ()内のURLにアクセスしたときに、その中の関数を実行
 """
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    users = User.query.all()
+    return render_template("index.html", users=users)
+
+@app.route('/new', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+
+        new_user = User(username=username, email=email)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except Exception as e: # エラーハンドリングを再度追加しました
+            db.session.rollback()
+            print(f"ユーザーの作成中にエラーが発生しました: {e}")
+            error_message = "そのユーザー名またはメールアドレスは既に登録されています。"
+            return render_template("create.html", error_message=error_message)
+    
+    return  render_template("create.html")
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete(id):
+    user_to_delete = User.query.get(id) # ユーザーが見つからない場合は404エラーを返す
+
+    db.session.delete(user_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 @app.route('/about')
 def about():
@@ -44,4 +76,5 @@ def show_user_profile(username):
     return f"{username}さんのページです"
 
 if __name__ == '__main__':
+    with app.app_context(): db.create_all()
     app.run(debug=True)         #   debug=Trueにすると開発時は色々いいらしい
